54da8e253a54004652d059f493b2622f52080a47
Khalifah Seni <94048500+khalifahseni@users.noreply.github.com>
2022-01-25 15:59:53 +0800

    Delete assets directory

--- assets/mbr-flip-card/mbr-flip-card.js
+++
@@ -1,2 +0,0 @@
-
-function bendBottomCorner(){$(".flip-card:not(.builderCard)").each(function(){$(this).addClass("builderCard")})}var isBuilder=$("html").hasClass("is-builder");if(isBuilder)$(document).on("add.cards",function(){0!=$(".flip-card").length&&bendBottomCorner()});
